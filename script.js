0d5960cda50e83c10eb3e5c3aabdb21e372e62f2
Zack O'Brien <zackobrien18@gmail.com>
2025-06-19 18:11:40 +1000

    tidying up

--- script.js
+++ script.js
@@ -2,11 +2,35 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
+	const heroHeading = document.getElementById('HeroHeading');
+	const heroByline = document.getElementById('HeroByline');
+	
     if (window.scrollY > lastScrollY) {
-        header.classList.add('hidden');
+        header.classList.add('headerHidden');
+		heroHeading.classList.add('hidden');
+		heroByline.classList.add('hidden');
     }
     else {
-        header.classList.remove('hidden');
+        header.classList.remove('headerHidden');
+		heroHeading.classList.remove('hidden');
+		heroByline.classList.remove('hidden');
     }
+	
     lastScrollY = window.scrollY;
 });
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('show');
+		}
+		else {
+			entry.target.classList.remove('show');
+		}
+	});
+}, {
+	threshold: 0.1
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el)); 
