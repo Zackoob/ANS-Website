21ef6ec538607c8354ad1ffca31229c7bbe0371b
Zack O'Brien <zackobrien18@gmail.com>
2025-06-29 22:48:50 +1000

    Applied code from laptop

--- script.js
+++ script.js
@@ -42,15 +42,16 @@ const sections = {
     ],
 }
 
-const contentBackground = document.getElementById('content-background');
-let currentSection = null;
+const contentBackground = document.getElementById('content-background'); // Get the background of the content
+
+let currentSection = 'hero'; // Current section for showOnly function - default 'hero' as page starts on hero
 
 function showOnly(sectionName) {
     if (!sectionName) return;
     if (currentSection == sectionName) return;
     currentSection = sectionName;
 
-
+	// Hide all text
     const allSections = Object.values(sections).flat();
     allSections.forEach(el => {
         if (el){
@@ -59,11 +60,13 @@ function showOnly(sectionName) {
         }
     });
 
+	// Change the background for the next element
     setTimeout(() => {
         contentBackground.classList.remove('welcome', 'vision', 'projects', 'hero');
         contentBackground.classList.add(sectionName);
     }, 750);
 
+	// Show the current element	
     setTimeout(() => {
         const visibleElements = sections[sectionName];
         if (!visibleElements) return;
@@ -71,10 +74,25 @@ function showOnly(sectionName) {
             if (el) {
                 el.classList.remove('hidden');
                 el.classList.add(sectionName);
+				hideAllExcept(sectionName);
             }
         });
     }, 1500);
-};
+}
+
+// Ensure all other elements are hidden
+function hideAllExcept(sectionName) {
+	if (!sectionName) return;
+	
+	for (const [name, element] of Object.entries(sections)) {
+		element.forEach(el => {
+			if (el) {
+				el.classList.toggle('hidden', name !== sectionName);
+				el.classList.toggle('visible', name == sectionName);
+			}
+		});
+	}
+}
 
 addEventListener('scroll', () => {
     // Scroll thresholds for elements to appear on screen 
@@ -83,19 +101,16 @@ addEventListener('scroll', () => {
     const visionThreshold = 1500;
     const projectThreshold = 2250;
 
-    
-
     // Check what threshold page scroll is in
     // Start from largest to smallest value threshold to work
-    if (scrollY > projectThreshold) {
-        showOnly('projects');
+    if (scrollY > projectThreshold) {		
+		showOnly('projects');
     }
     else if (scrollY > visionThreshold) {
-        showOnly('vision');
+    	showOnly('vision'); 
     }
     else if (scrollY > welcomeThreshold) {
         showOnly('welcome');
-
     }
     else {
         showOnly('hero');
